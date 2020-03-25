7bda972d5fda2c1a93b875346c5d6fddd81de4ab
David Steinsland <david@steinsland.email>
2020-03-25 12:11:50 +0100

    oppdaterer rapids-and-rivers

--- build.gradle.kts
+++ build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 group = "no.nav.helse"
 
 dependencies {
-    implementation("com.github.navikt:rapids-and-rivers:1.84a7ce0")
+    implementation("com.github.navikt:rapids-and-rivers:1.1822995")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
     implementation("io.ktor:ktor-client-jackson:$ktorVersion")
 
